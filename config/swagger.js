584a80408e09f4f97607f351deccf6d88b4d1576
ALish <sharipkhanalikhan@gmail.com>
2025-12-26 19:09:17 +0500

    Revert "Football"
    This reverts commit 3e4ab996329bd98e5c2e5b8ae1931cb7591fed11.

--- config/swagger.js
+++ config/swagger.js
@@ -19,8 +19,13 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000/api/v1/",
+<<<<<<< HEAD
+        url: "http://localhost:3000/api/v1",
         description: "Development Server",
+=======
+        url: 'http://localhost:3000/',
+        description: 'Development Server',
+>>>>>>> 45419a03ee6667dde46943c4bb77b6f57ea9e063
       },
     ],
     components: {
