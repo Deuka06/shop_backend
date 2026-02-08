c1f9b23c1afab0a65350b4014ad1a229b535db41
ALish <sharipkhanalikhan@gmail.com>
2026-02-08 17:56:58 +0500

    Added Patch method

--- src/server.js
+++ src/server.js
@@ -30,7 +30,7 @@ app.use(
   cors({
     origin: "http://localhost:5173",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
   }),
 );
